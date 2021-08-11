96a602ce9388cdde7c6a788f1d334d2117dc98f1
Daniel Marin <danielmarin998@hotmail.com>
2021-08-11 14:04:10 -0500

    Herencia en user y driver con JavaScript.

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,9 @@ var car = new Car("AW456", new Account("Andres Hernandez", "AND123"));
 car.passenger = 4;
 car.printDataCar();
 
-var uberX = new uberX("AW456", new Account("Andrea Ferran", "ANDA789"), "Chevrolet", "Spark")
+var uberX = new UberX("AW456", new Account("Andrea Ferran", "ANDA789"), "Chevrolet", "Spark");
 uberX.passenger = 4;
 uberX.printDataCar();
+
+var user =  new User("Damian Rios", "ASD098", "damian98@platzi.co", "ehwdi09wjqd9");
+user.printDataUser();
