5c669dfca2f13d787c5fbbb29dd3a13e9931c821
Daniel Marin <danielmarin998@hotmail.com>
2021-08-11 13:53:05 -0500

    se testeo la herencia en JavaScript

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Hernandez", "AND123"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new uberX("AW456", new Account("Andrea Ferran", "ANDA789"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
