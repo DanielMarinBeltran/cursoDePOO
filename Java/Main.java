e91857620697936cbf14e4500334ea4231c5fe95
Daniel Marin <danielmarin998@hotmail.com>
2021-08-11 15:45:24 -0500

    Se aplico polimorfirmo a Java.

--- Java/Main.java
+++ Java/Main.java
@@ -3,12 +3,15 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Spark");
+        uberX.setPassenger(4);
+        uberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AHD123"));
-        car2.passenger = 3;
-        car.printDataCar();
+        UberVan uberVan = new UberVan("FGH345", new Account("Andres Herrera", "AND123"));
+        uberVan.setPassenger(6);
+        uberVan.printDataCar();
+        // Car car2 = new Car("QWE567", new Account("Andrea Herrera", "AHD123"));
+        // car2.passenger = 3;
+        // car2.printDataCar();
     }
 }
